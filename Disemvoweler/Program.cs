fc16fcee5ce2d4c8fe89c88e8b321fbe06542664
qwertydog <campbelldeas@hotmail.com>
2016-05-14 14:44:14 +1200

    Completed solution

--- Disemvoweler/Program.cs
+++ Disemvoweler/Program.cs
@@ -10,6 +10,37 @@ namespace Disemvoweler
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
+
+            Disemvowel(input);
+
+            Console.ReadKey();
+        }
+
+        static void Disemvowel(string input)
+        {
+            string vowels = "aeiou";
+            string inputVowels = string.Empty;
+            string inputConsonants = string.Empty;
+
+            foreach (var letter in input)
+            {
+                if (letter == ' ')
+                {
+                    continue;
+                }
+                else if (vowels.Contains(letter))
+                {
+                    inputVowels += letter;
+                }
+                else
+                {
+                    inputConsonants += letter;
+                }
+            }
+
+            Console.WriteLine(inputConsonants);
+            Console.WriteLine(inputVowels);
         }
     }
 }
